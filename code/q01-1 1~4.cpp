301f8ba965727d253d8ae3412cd16165f90cfde1
okboseong <okboseong@gmail.com>
2021-01-30 00:53:54 +0900

    q01

---
+++ code/q01-1 1~4.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+//int main(void) {
+//	int num1, num2, num3, num4, num5;
+//	int result = 0;
+//
+//	std::cout << "1번째 정수 입력: ";
+//	std::cin >> num1;
+//
+//	std::cout << "2번째 정수 입력: ";
+//	std::cin >> num2;
+//
+//	std::cout << "3번째 정수 입력: ";
+//	std::cin >> num3;
+//
+//	std::cout << "4번째 정수 입력: ";
+//	std::cin >> num4;
+//
+//	std::cout << "5번째 정수 입력: ";
+//	std::cin >> num5;
+//
+//	result = num1 + num2 + num3 + num4 + num5;
+//
+//	std::cout << "합계: "<<result;
+//
+//}
+
+//int main(void) {
+//	char name[10];
+//	char phone[15];
+//
+//	std::cout << "your name: ";
+//	std::cin >> name;
+//
+//	std::cout << "your phone num: ";
+//	std::cin >> phone;
+//
+//	return 0;
+//}
+
+//int main(void) {
+//	int num1;
+//
+//	std::cin >> num1;
+//
+//	for (int i = 1; i < 10; i++) {
+//		std::cout << num1 << " * " << i << " = " << num1 * i << std::endl;
+//
+//	}
+//
+//
+//}
+
+//int main(void) {
+//
+//	int basic = 50, sal, sum =0;
+//	
+//	while (1) {
+//
+//		std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+//		std::cin >> sal;
+//		
+//		if (sal == -1) {
+//			std::cout << "프로그램을 종료합니다." << std::endl;
+//			break;
+//		}
+//		sum = basic + sal * 0.12;
+//
+//		std::cout << "이번달 급여: " << sum << std::endl;
+//
+//	}
+//}
